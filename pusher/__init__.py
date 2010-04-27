8281b431535f30b90df8f096f1a663669acaed73
Pablo Brasero <pablo@new-bamboo.co.uk>
2010-04-27 11:50:06 +0100

    Instance-specific parameters

--- pusher/__init__.py
+++ pusher/__init__.py
@@ -3,7 +3,8 @@ key    = None
 secret = None
 
 class Pusher(object):
-    def __init__(self):
-        self.app_id = app_id
-        self.key = key
-        self.secret = secret
+    def __init__(self, app_id=None, key=None, secret=None):
+        _globals = globals()
+        self.app_id = app_id or _globals['app_id']
+        self.key = key or _globals['key']
+        self.secret = secret or _globals['secret']
--- pusher/test.py
+++ pusher/test.py
@@ -7,19 +7,42 @@ class PropertiesTest(unittest.TestCase):
         pusher.app_id = 'test-global-app-id'
         pusher.key = 'test-global-key'
         pusher.secret = 'test-global-secret'
-        self.i = pusher.Pusher()
 
     def tearDown(self):
         pusher.app_id = 'api.pusherapp.com'
         pusher.key = None
         pusher.secret = None
 
-    def test_global_app_id(self):
-        eq_(self.i.app_id, 'test-global-app-id')
+    #
+    # Using globals
+    #
+
+    def _default_instance(self):
+        return pusher.Pusher()
+
+    def test_global_app_id(self, *args):
+        eq_(self._default_instance(*args).app_id, 'test-global-app-id')
 
     def test_global_key(self):
-        eq_(self.i.key, 'test-global-key')
+        eq_(self._default_instance().key, 'test-global-key')
 
     def test_global_secret(self):
-        eq_(self.i.secret, 'test-global-secret')
+        eq_(self._default_instance().secret, 'test-global-secret')
+
+
+    #
+    # Using instance-specific parameters
+    #
+
+    def _instance(*args):
+        return pusher.Pusher(app_id='test-instance-app-id', key='test-instance-key', secret='test-instance-secret')
+
+    def test_instance_app_id(self):
+        eq_(self._instance().app_id, 'test-instance-app-id')
+
+    def test_instance_key(self):
+        eq_(self._instance().key, 'test-instance-key')
+
+    def test_instance_secret(self):
+        eq_(self._instance().secret, 'test-instance-secret')
 
