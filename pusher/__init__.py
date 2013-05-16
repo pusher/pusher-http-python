3f358cb2c7257aa2a797ebc49def52bb21b3616e
Daniel Waterworth <Da.Waterworth@gmail.com>
2013-05-16 15:01:39 +0100

    The default encoder should be None

--- pusher/__init__.py
+++ pusher/__init__.py
@@ -47,7 +47,7 @@ def pusher_from_url(url=None):
     return Pusher(**url2options(url))
 
 class Pusher(object):
-    def __init__(self, app_id=None, key=None, secret=None, host=None, port=None, encoder=json):
+    def __init__(self, app_id=None, key=None, secret=None, host=None, port=None, encoder=None):
         _globals = globals()
         self.app_id = app_id or _globals['app_id']
         self.key = key or _globals['key']
