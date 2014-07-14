118803f6a9006d416c95d6a5fa00732f42327902
Daniel Waterworth <Da.Waterworth@gmail.com>
2014-07-14 11:05:48 +0100

    Added a requests backend

---
+++ pusher/requests.py
@@ -0,0 +1,20 @@
+from __future__ import print_function, unicode_literals, absolute_import, division
+
+import requests
+
+from pusher.util import *
+
+class RequestsBackend(object):
+  def __init__(self, config, **options):
+    self.options = {'verify': True}
+    self.options.update(options)
+
+  def send_request(self, request):
+    resp = requests.request(
+      request.method,
+      request.url,
+      headers=request.headers,
+      data=request.body,
+      **self.options
+    )
+    return process_response(resp.status_code, resp.text)
