7df67494b62bbed366d630586b38b2aae584dc05
Daniel Waterworth <Da.Waterworth@gmail.com>
2014-07-14 11:06:27 +0100

    Added an asyncio backend

---
+++ pusher/aiohttp.py
@@ -0,0 +1,19 @@
+import pusher
+import aiohttp
+
+class AsyncIOBackend:
+  def __init__(self, config):
+    self.config = config
+    if config.ssl:
+      raise NotImplementedError("SSL not supported for this backend")
+
+  def send_request(self, request):
+    method = request.method
+    url = "http://%s:%s%s" % (self.config.host, self.config.port, request.path)
+    params = request.query_params
+    data = request.body
+    headers = request.headers
+
+    response = yield from aiohttp.request(method, url, params=params, data=data, headers=headers)
+    body = yield from response.read_and_close()
+    return pusher.process_response(response.status, body.decode('utf8'))
