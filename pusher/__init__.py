242713db6a9148a09243773bddf8f2673b1365ae
Joey Rosztoczy <joeyr@stotzeq.com>
2015-01-13 23:45:33 -0500

    urlparse was breaking python3+ compat

--- pusher/__init__.py
+++ pusher/__init__.py
@@ -9,12 +9,11 @@ import hmac
 import json
 import hashlib
 try:
-    from urllib.parse import quote
+    import urllib.parse as urlparse
 except ImportError:
-    from urllib import quote
+    import urlparse
 import re
 import socket
-import urlparse
 
 if sys.version < '3':
     text_type = unicode
