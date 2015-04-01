8fa7f44b3b7de08664b5133f968df8173d029284
zimbatm <zimbatm@zimbatm.com>
2015-04-01 10:50:29 +0100

    Add timeout and Keep-Alive to the aiohttp adapter

--- pusher/aiohttp.py
+++ pusher/aiohttp.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import aiohttp
+import asyncio
 
 from pusher.http import process_response
 
 class AsyncIOBackend:
-    def __init__(_, _):
+    def __init__(self, config):
         """Adapter for the requests module.
 
         :param config:  pusher.Pusher object
         """
+        self.config = config
+        self.conn = aiohttp.TCPConnector()
 
     def send_request(_, request):
         method = request.method
@@ -18,6 +21,9 @@ class AsyncIOBackend:
         data = request.body
         headers = request.headers
 
-        response = yield from aiohttp.request(method, url, params=params, data=data, headers=headers)
+        response = yield from asyncio.wait_for(
+            aiohttp.request(method, url, params=params, data=data, headers=headers, connector=self.conn),
+            timeout=self.config.timeout
+        )
         body = yield from response.read_and_close()
         return process_response(response.status, body.decode('utf8'))
