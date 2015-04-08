2c9570d97cc8e1e2007da1dc3f8958f4aa789a3f
zimbatm <zimbatm@zimbatm.com>
2015-04-08 22:26:01 +0100

    Add trigger examples

---
+++ trigger_tornado.py
@@ -0,0 +1,19 @@
+import pusher
+import pusher.tornado
+import tornado.ioloop
+
+ioloop = tornado.ioloop.IOLoop.instance()
+
+def show_response(response):
+    print(response.result())
+    ioloop.stop()
+
+client = pusher.Pusher.from_env(
+            backend=pusher.tornado.TornadoBackend,
+            timeout=50
+         )
+response = client.trigger("hello", "world", dict(foo='bar'))
+response.add_done_callback(show_response)
+print("Before start")
+ioloop.start()
+print("After start")
