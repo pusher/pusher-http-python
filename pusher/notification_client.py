4a8dd97cd90ce9f5b599aa9c357f67698f7d558e
Jamie Patel <jamie@notespublication.com>
2016-07-06 15:14:49 +0100

    Add validation

--- pusher/notification_client.py
+++ pusher/notification_client.py
@@ -1,6 +1,13 @@
 from .config import Config
 from .http import POST, Request, request_method
 
+DEFAULT_HOST = "yolo.ngrok.io"
+RESTRICTED_GCM_KEYS = ['to', 'registration_ids']
+API_PREFIX = '/customer_api'
+API_VERSION = '/v1'
+GCM_TTL = 241920
+WEBHOOK_LEVELS = ['INFO', 'DEBUG', '']
+
 class NotificationClient(Config):
 
 	def __init__(self, app_id, key, secret, ssl=True, host=None, port=None, timeout=5, cluster=None,
@@ -15,13 +22,64 @@ class NotificationClient(Config):
 		if host:
 			self._host = ensure_text(host, "host")
 		else:
-			self._host = "yolo.ngrok.io"
+			self._host = DEFAULT_HOST
 
 
 	@request_method
 	def notify(self, interests, notification):
-	    params = {
-	        'interests': interests,
-	    }
-	    params.update(notification)
-	    return Request(self, POST, "/customer_api/v1/apps/%s/notifications" % self.app_id, params)
+		if not isinstance(interests, list) and not isinstance(interests, set):
+			raise TypeError("Interests must be a list or a set")
+
+		if not isinstance(notification, dict):
+			raise TypeError("Notification must be a dictionary")
+
+		params = {
+		    'interests': interests,
+		}
+		params.update(notification)
+		self.validate_notification(params)
+		return Request(self, POST, "/customer_api/v1/apps/%s/notifications" % self.app_id, params)
+
+	def validate_notification(self, notification):
+		gcm_payload = notification.get('gcm')
+
+		if not gcm_payload and not notification.get('apns') :
+			raise ValueError("Notification must have fields APNS or GCM")
+
+		if gcm_payload:
+			for restricted_key in RESTRICTED_GCM_KEYS:
+				gcm_payload.pop(restricted_key, None)
+
+			ttl = gcm_payload.get('time_to_live')
+			if ttl:
+				if not isinstance(ttl, int):
+					raise ValueError("GCM time_to_live must be an int")
+
+				if not (0 <= ttl <= GCM_TTL):
+					raise ValueError("GCM time_to_live must be between 0 and 241920 (4 weeks)")
+
+			gcm_payload_notification = gcm_payload.get('notification')
+			if gcm_payload_notification:
+				title = gcm_payload_notification.get('title')
+				icon = gcm_payload_notification.get('icon')
+				if not isinstance(title, str):
+					raise ValueError("GCM notification title is required must be a string")
+
+				if not isinstance(icon, str):
+					raise ValueError("GCM notification icon is required must be a string")
+
+				if len(title) is 0:
+					raise ValueError("GCM notification title must not be empty")
+
+				if len(icon) is 0:
+					raise ValueError("GCM notification icon must not be empty")
+
+		webhook_url = notification.get('webhook_url')
+		webhook_level = notification.get('webhook_level')
+
+		if webhook_level:
+			 if not webhook_url:
+				raise ValueError("webhook_level cannot be used without a webhook_url")
+
+			 if not webhook_level in WEBHOOK_LEVELS:
+				raise ValueError("webhook_level must be either INFO or DEBUG. Blank will default to INFO")
