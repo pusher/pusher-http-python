438a4db665d3bb7cdcbe0bd4956de37870836a0d
James Fisher <jameshfisher@gmail.com>
2016-08-02 14:14:38 +0100

    polyfill for assertRegexpMatches in Python 2.6

--- pusher_tests/test_request.py
+++ pusher_tests/test_request.py
@@ -3,6 +3,7 @@
 from __future__ import print_function, absolute_import, division
 
 import unittest
+import re
 
 from pusher import Pusher
 from pusher.http import Request
@@ -48,6 +49,20 @@ class TestRequest(unittest.TestCase):
                 self.assertEqual(req.query_params, expected)
 
             json_dumps_mock.assert_called_once_with({u"foo": u"bar"})
+
+    # Copied wholesale from https://github.com/python/cpython/blob/2d305e1c46abfcd609bf8b2dff8d2065e6af8ab2/Lib/unittest/case.py#L1279-L1289
+    # This can be removed when we no longer support Python 2.6
+    def assertRegexpMatches(self, text, expected_regex, msg=None):
+        """Fail the test unless the text matches the regular expression."""
+        if isinstance(expected_regex, (str, bytes)):
+            assert expected_regex, "expected_regex must not be empty."
+            expected_regex = re.compile(expected_regex)
+        if not expected_regex.search(text):
+            standardMsg = "Regex didn't match: %r not found in %r" % (
+                expected_regex.pattern, text)
+            # _formatMessage ensures the longMessage option is respected
+            msg = self._formatMessage(msg, standardMsg)
+            raise self.failureException(msg)
 
     def test_x_pusher_library_header(self):
         conf = Pusher.from_url(u'http://key:secret@somehost/apps/4')
