0f58ff0a54835acf525e973d68ff1417c33f24a6
Callum Oakley <c.oakley108@gmail.com>
2016-09-04 16:14:47 +0100

    more cleaning

--- pusher/notification_client.py
+++ pusher/notification_client.py
@@ -1,6 +1,15 @@
-from .client import Client
-from .http import POST, Request, request_method
-from .util import ensure_text
+# -*- coding: utf-8 -*-
+
+from __future__ import (
+    print_function,
+    unicode_literals,
+    absolute_import,
+    division)
+
+from pusher.client import Client
+from pusher.http import POST, Request, request_method
+from pusher.util import ensure_text
+
 
 DEFAULT_HOST = "nativepush-cluster1.pusher.com"
 RESTRICTED_GCM_KEYS = ['to', 'registration_ids']
@@ -9,17 +18,19 @@ API_VERSION = 'v1'
 GCM_TTL = 241920
 WEBHOOK_LEVELS = ['INFO', 'DEBUG', '']
 
+
 class NotificationClient(Client):
-    def __init__(self, app_id, key, secret, ssl=True, host=None, port=None, timeout=5, cluster=None,
-                 json_encoder=None, json_decoder=None, backend=None, **backend_options):
+    def __init__(
+            self, app_id, key, secret, ssl=True, host=None, port=None,
+            timeout=5, cluster=None, json_encoder=None, json_decoder=None,
+            backend=None, **backend_options):
         super(NotificationClient, self).__init__(
-            app_id, key, secret, ssl,
-            host, port, timeout, cluster,
-            json_encoder, json_decoder, backend,
-            **backend_options)
+            app_id, key, secret, ssl, host, port, timeout, cluster,
+            json_encoder, json_decoder, backend, **backend_options)
 
         if host:
             self._host = ensure_text(host, "host")
+
         else:
             self._host = DEFAULT_HOST
 
@@ -36,8 +47,10 @@ class NotificationClient(Client):
             raise TypeError("Notification must be a dictionary")
 
         params = {
-            'interests': interests,
-        }
+            'interests': interests}
+
         params.update(notification)
-        path =  "/%s/%s/apps/%s/notifications" % (API_PREFIX, API_VERSION, self.app_id)
+        path = "/%s/%s/apps/%s/notifications" %
+            (API_PREFIX, API_VERSION, self.app_id)
+
         return Request(self, POST, path, params)
